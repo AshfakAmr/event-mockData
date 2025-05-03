49d784a5597f908ad0b9b3b7406623728f31ccb2
Ashfak Ahamed <ashfakahamed222@gmail.com>
2025-05-03 16:25:17 +0530

    s added cors and server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 8080;
+
+// Enable CORS
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
